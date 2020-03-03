920498e8bcb1414330590f555d3a6f62075e61a1
Syndamia <kamen.d.mladenov@protonmail.com>
2020-03-03 20:40:05 +0200

    Transfered project from IT-kariera repository to this one

---
+++ Mundus/Models/SuperLayers/ISuperLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Mundus.Models.Tiles;
+using Gtk;
+
+namespace Mundus.Models.SuperLayers {
+    public interface ISuperLayer {
+        ItemTile GetItemTileType(string name);
+        GroundTile GetGroundTileType(string name);
+
+        ItemTile GetItemLayerTile(int yPos, int xPos);
+        GroundTile GetGroundLayerTile(int yPos, int xPos);
+
+        void SetItemLayer(ItemTile[,] itemTiles);
+        void SetGroundLayer(GroundTile[,] groundTiles);
+    }
+}
---
+++ Mundus/Views/Windows/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+using Mundus.Models;
+
+namespace Mundus.Views.Windows {
+    public partial class MainWindow : Gtk.Window {
+        public MainWindow() : base( Gtk.WindowType.Toplevel ) {
+            this.Build();
+        }
+
+        private void OnDeleteEvent(object sender, DeleteEventArgs a) {
+            Application.Quit();
+        }
+
+        private void OnBtnNewGameClicked(object sender, EventArgs e) {
+            this.Hide();
+            WindowInstances.WNewGame.SetDefaults();
+            WindowInstances.WNewGame.Show();
+        }
+
+        private void OnBtnSettingsClicked(object sender, EventArgs e) {
+            this.Hide();
+            WindowInstances.WSettings.Show(this);
+        }
+
+        protected void OnBtnTutorialClicked(object sender, EventArgs e) {
+        }
+    }
+}
