d580ea82d48831e3cf243e5b250bf7076c8a6b1b
Syndamia <kamen.d.mladenov@protonmail.com>
2020-05-19 13:29:09 +0300

    Added final data test (that I didn't think I could do)

---
+++ MundusTests/DataTests/Mobs/MITests.cs
@@ -0,0 +1,27 @@
+﻿namespace MundusTests.DataTests.Mobs 
+{
+    using Gtk;
+    using Mundus.Data;
+    using Mundus.Data.Tiles.Mobs;
+    using Mundus.Data.Windows;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public static class MITests 
+    {
+        [SetUp]
+        public static void SetUp() 
+        {
+            Application.Init();
+            DataBaseContexts.CreateInstances();
+            WI.CreateInstances();
+            WI.WNewGame.OnBtnGenerateClicked(null, null);
+        }
+
+        [Test]
+        public static void CreatesPlayerInstance() 
+        {
+            Assert.AreNotEqual(null, MI.Player, "Player isn't instantiated");
+        }
+    }
+}
---
+++ MundusTests/ServiceTests/SuperLayers/HeightControllerTests.cs
@@ -0,0 +1,30 @@
+﻿namespace MundusTests.ServiceTests.SuperLayers 
+{
+    using Mundus.Data;
+    using Mundus.Service.SuperLayers;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public static class HeightControllerTests 
+    {
+        [Test]
+        public static void GetsCorrectSuperLayerUnderneath() 
+        {
+            DataBaseContexts.CreateInstances();
+
+            Assert.AreEqual(DataBaseContexts.LContext, HeightController.GetSuperLayerUnderneath(DataBaseContexts.SContext), "GetSuperLayerUnderneath doesn't return that land is below sky");
+            Assert.AreEqual(DataBaseContexts.UContext, HeightController.GetSuperLayerUnderneath(DataBaseContexts.LContext), "GetSuperLayerUnderneath doesn't return that underground is below land");
+            Assert.AreEqual(null, HeightController.GetSuperLayerUnderneath(DataBaseContexts.UContext), "GetSuperLayerUnderneath doesn't return that there is nothing (null) below underground");
+        }
+
+        [Test]
+        public static void GetsCorrectSuperLayerAbove() 
+        {
+            DataBaseContexts.CreateInstances();
+
+            Assert.AreEqual(null, HeightController.GetSuperLayerAbove(DataBaseContexts.SContext), "GetSuperLayerUnderneath doesn't return that there is nothing (null) above sky");
+            Assert.AreEqual(DataBaseContexts.SContext, HeightController.GetSuperLayerAbove(DataBaseContexts.LContext), "GetSuperLayerUnderneath doesn't return that sky is above land");
+            Assert.AreEqual(DataBaseContexts.LContext, HeightController.GetSuperLayerAbove(DataBaseContexts.UContext), "GetSuperLayerUnderneath doesn't return that land is above underground");
+        }
+    }
+}
